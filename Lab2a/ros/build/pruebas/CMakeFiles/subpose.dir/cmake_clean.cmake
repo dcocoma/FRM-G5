e57c6d28afe3be1f3c35ee4d63236ea37cb82cbe
ElJoho <johalopezari@unal.edu.co>
2024-04-28 15:21:11 -0500

    Add files via upload

---
+++ Lab2a/ros/build/pruebas/CMakeFiles/subpose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/eljoho/Desktop/ros/devel/lib/pruebas/subpose"
+  "/home/eljoho/Desktop/ros/devel/lib/pruebas/subpose.pdb"
+  "CMakeFiles/subpose.dir/subpose.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/subpose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
