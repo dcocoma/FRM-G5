309195c3cb42fc57e861ec8729e0406d6f20a968
ElJoho <johalopezari@unal.edu.co>
2024-04-28 15:03:52 -0500

    Create CTestTestfile.cmake

---
+++ Lab2a/ros/build/pruebas/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eljoho/Desktop/ros/src/pruebas
+# Build directory: /home/eljoho/Desktop/ros/build/pruebas
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
