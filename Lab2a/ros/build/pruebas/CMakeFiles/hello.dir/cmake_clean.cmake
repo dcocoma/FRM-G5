0e5bce730a1d5f31e1fff06c9432c954f1969cd0
ElJoho <johalopezari@unal.edu.co>
2024-04-28 19:43:27 -0500

    Add files via upload

---
+++ Lab2a/ros/build/pruebas/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/eljoho/Desktop/ros/devel/lib/pruebas/hello"
+  "/home/eljoho/Desktop/ros/devel/lib/pruebas/hello.pdb"
+  "CMakeFiles/hello.dir/hello.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
